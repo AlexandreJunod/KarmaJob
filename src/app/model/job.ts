3d1ff2bd81bfca2ae37a6a0ff0d46ccc7fc691fc
AlexandreJunod <36194606+AlexandreJunod@users.noreply.github.com>
2019-06-17 22:40:17 +0200

    DataProvider for Statuses and Users

--- src/app/model/job.ts
+++ src/app/model/job.ts
@@ -10,7 +10,7 @@ export class Job {
     worker: number
     status_id: number
 
-    constructor(id: number, title: string, description: string, theme: string, date: string, duration: number, karmapoints: number, owner: number, status_id: number) {
+    constructor(id: number, title: string, description: string, theme: string, date: string, duration: number, karmapoints: number, owner: number, worker: number, status_id: number) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -19,6 +19,7 @@ export class Job {
         this.duration = duration;
         this.karmapoints = karmapoints;
         this.owner = owner;
+        this.worker = worker;
         this.status_id = status_id;
     }
 
